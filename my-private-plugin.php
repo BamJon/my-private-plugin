e87d47b2123b49bd1fd3bc225adba84592e4555d
BamJon <97885341+BamJon@users.noreply.github.com>
2025-01-29 10:12:39 +0000

    Update my-private-plugin.php

--- my-private-plugin.php
+++ my-private-plugin.php
@@ -3,7 +3,7 @@
 Plugin Name: Test plugin
 Plugin URI: https://github.com/BamJon/my-private-plugin
 Description: A custom plugin with GitHub updates.
-Version: 1.0.3
+Version: 1.0.4
 Author: jon
 Author URI: https://yourwebsite.com
 License: GPL2
